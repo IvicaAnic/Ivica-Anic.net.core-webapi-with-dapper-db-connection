c949a7cbec38a9aa80004af4ff39d356f69950c1
IvicaAnic <68137929+IvicaAnic@users.noreply.github.com>
2021-12-24 13:23:49 +0100

    first commit

---
+++ model/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookWebAPI.Model
+{
+    public class Author
+    {
+        public string AuthorName { get; set; }
+        public string AuthorVorname { get; set; }
+        public string Strasse { get; set; }
+        public string PLZ { get; set; }
+        public string Stadt { get; set; }
+    }
+    public class Book
+    {
+        public string BookTitle { get; set; }
+        public float BookPrice { get; set; }
+        public int AuthorID { get; set; }
+        public string BookVerlag { get; set; }
+        public string BookISDN { get; set; }
+
+    }
+}
